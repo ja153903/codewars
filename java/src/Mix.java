b8cae91d9337442fd111aa29c77ec26f20adb6ce
jaimeabbariao <jaimeabbariao@gmail.com>
2018-08-11 00:23:42 -0400

    Implemented Mix.java and Mumbling.java

--- java/src/Mix.java
+++ java/src/Mix.java
@@ -1,4 +1,99 @@
-package PACKAGE_NAME;
+import java.util.*;
+
+class ResultComponents implements Comparable<ResultComponents> {
+    public char USER; // =, 1, 2
+    public int MAX_FREQUENCY;
+    public char CHAR;
+
+    public ResultComponents(char user, int maxFrequency, char char_) {
+        USER = user;
+        MAX_FREQUENCY = maxFrequency;
+        CHAR = char_;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s:", USER));
+        for (int i = 0; i < MAX_FREQUENCY; i++) {
+            sb.append(CHAR);
+        }
+        return sb.toString();
+
+    }
+
+    @Override
+    public int compareTo(ResultComponents resultComponents) {
+        if (this.MAX_FREQUENCY < resultComponents.MAX_FREQUENCY) {
+            return 1;
+        } else if (this.MAX_FREQUENCY > resultComponents.MAX_FREQUENCY) {
+            return -1;
+        }
+
+        // if not, then there's a tie. so we go to the next attribute
+        if (this.USER < resultComponents.USER) {
+            return -1;
+        } else if (this.USER > resultComponents.USER) {
+            return 1;
+        }
+
+        // next we check the character
+        
+        if (this.CHAR < resultComponents.CHAR) {
+            return -1;
+        } else if (this.CHAR > resultComponents.CHAR) {
+            return 1;
+        }
+
+        return 0;
+
+    }
+}
 
 public class Mix {
+    // We can create another class to hold the components of the
+    // desired string and then implement a comparator based on it
+    // comparator order is: freq, user, character
+    
+    public static int[] getAsciiArray(String s) {
+        int[] ascii = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isLetter(currentChar) && Character.isLowerCase(currentChar))
+                ascii[currentChar - 97]++;
+        }
+        return ascii;
+    }
+
+    public static String mix(String s1, String s2) {
+        if (s1.equals(s2)) return "";
+
+        int[] s1A = getAsciiArray(s1);
+        int[] s2A = getAsciiArray(s2);
+
+        ArrayList<ResultComponents> objs = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            if (s1A[i] > 1 || s2A[i] > 1) {
+                if (s1A[i] == s2A[i]) {
+                    objs.add(new ResultComponents('=', s1A[i], (char) (i + 97)));
+                } else if (s1A[i] > s2A[i]) {
+                    objs.add(new ResultComponents('1', s1A[i], (char) (i + 97)));
+                } else {
+                    objs.add(new ResultComponents('2', s2A[i], (char) (i + 97)));
+                }
+            }
+        }
+
+        Collections.sort(objs);
+        return objs.stream()
+                .map(ResultComponents::toString)
+                .reduce((prev, cur) -> prev + "/" + cur).get();
+    }
+
+    public static void main(String[] args) {
+//        assertEquals("2:eeeee/2:yy/=:hh/=:rr", Mixing.mix("Are they here", "yes, they are here"))
+        System.out.println(mix("Are they here", "yes, they are here"));
+    }
+
 }
--- java/src/Mumbling.java
+++ java/src/Mumbling.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Mumbling {
+    public static String accum(String s) {
+        String[] sArr = s.split("");
+        // at index i, we want i+1 copies of the same string
+        String[] result = new String[sArr.length];
+        for (int i = 0; i < sArr.length; i++) {
+            String str = sArr[i].toLowerCase();
+            StringBuilder temp = new StringBuilder();
+            for (int j = 0; j < i+1; j++) {
+                if (j == 0) {
+                    temp.append(str.toUpperCase());
+                } else {
+                    temp.append(str);
+                }
+            }
+            result[i] = temp.toString();
+        }
+
+        return String.join("-", result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(accum("abcd"));
+    }
 }
